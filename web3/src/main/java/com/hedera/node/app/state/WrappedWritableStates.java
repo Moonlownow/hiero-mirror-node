af91412824ee69fbc5a6432d74bfce69517b3bda
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-06-16 13:55:10 +0300

    temp
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

---
+++ web3/src/main/java/com/hedera/node/app/state/WrappedWritableStates.java
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package com.hedera.node.app.state;
+
+import static java.util.Objects.requireNonNull;
+
+import com.swirlds.state.spi.CommittableWritableStates;
+import com.swirlds.state.spi.WrappedWritableKVState;
+import com.swirlds.state.spi.WrappedWritableQueueState;
+import com.swirlds.state.spi.WrappedWritableSingletonState;
+import com.swirlds.state.spi.WritableKVState;
+import com.swirlds.state.spi.WritableQueueState;
+import com.swirlds.state.spi.WritableSingletonState;
+import com.swirlds.state.spi.WritableStates;
+import jakarta.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.hiero.mirror.web3.common.ContractCallContext;
+
+/**
+ * A wrapper around a {@link WritableStates} that provides a wrapped instances of {@link WritableKVState} and
+ * {@link WritableSingletonState} of a given {@link WritableStates} delegate.
+ */
+public class WrappedWritableStates implements WritableStates {
+
+    private final WritableStates delegate;
+    private final Map<String, WrappedWritableKVState<?, ?>> writableKVStateMap = new HashMap<>();
+    private final Map<String, WrappedWritableSingletonState<?>> writableSingletonStateMap = new HashMap<>();
+    private final Map<String, WrappedWritableQueueState<?>> writableQueueStateMap = new HashMap<>();
+
+    /**
+     * Constructs a {@link WrappedWritableStates} that wraps the given {@link WritableStates}.
+     *
+     * @param delegate the {@link WritableStates} to wrap
+     * @throws NullPointerException if {@code delegate} is {@code null}
+     */
+    public WrappedWritableStates(@Nonnull final WritableStates delegate) {
+        this.delegate = requireNonNull(delegate, "delegate must not be null");
+    }
+
+    @Override
+    public boolean contains(@Nonnull String stateKey) {
+        return delegate.contains(stateKey);
+    }
+
+    @Override
+    @Nonnull
+    public Set<String> stateKeys() {
+        return delegate.stateKeys();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    @Nonnull
+    public <K, V> WritableKVState<K, V> get(@Nonnull String stateKey) {
+        return (WritableKVState<K, V>) writableKVStateMap.computeIfAbsent(
+                stateKey,
+                s -> new WrappedWritableKVState<>(
+                        delegate.get(stateKey),
+                        ContractCallContext.get().getReadCacheState(stateKey),
+                        ContractCallContext.get().getWriteCacheState(stateKey)));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    @Nonnull
+    public <T> WritableSingletonState<T> getSingleton(@Nonnull String stateKey) {
+        return (WritableSingletonState<T>) writableSingletonStateMap.computeIfAbsent(
+                stateKey, s -> new WrappedWritableSingletonState<>(delegate.getSingleton(stateKey)));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    @Nonnull
+    public <E> WritableQueueState<E> getQueue(@Nonnull String stateKey) {
+        return (WritableQueueState<E>) writableQueueStateMap.computeIfAbsent(
+                stateKey, s -> new WrappedWritableQueueState<>(delegate.getQueue(stateKey)));
+    }
+
+    /**
+     * Returns {@code true} if the state of this {@link WrappedWritableStates} has been modified.
+     *
+     * @return {@code true}, if the state has been modified; otherwise {@code false}
+     */
+    public boolean isModified() {
+        for (WrappedWritableKVState<?, ?> kvState : writableKVStateMap.values()) {
+            if (kvState.isModified()) {
+                return true;
+            }
+        }
+        for (WrappedWritableQueueState<?> queueState : writableQueueStateMap.values()) {
+            if (queueState.isModified()) {
+                return true;
+            }
+        }
+        for (WrappedWritableSingletonState<?> singletonState : writableSingletonStateMap.values()) {
+            if (singletonState.isModified()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Writes all modifications to the underlying {@link WritableStates}.
+     */
+    public void commit() {
+        for (WrappedWritableKVState<?, ?> kvState : writableKVStateMap.values()) {
+            kvState.commit();
+        }
+        for (WrappedWritableQueueState<?> queueState : writableQueueStateMap.values()) {
+            queueState.commit();
+        }
+        for (WrappedWritableSingletonState<?> singletonState : writableSingletonStateMap.values()) {
+            singletonState.commit();
+        }
+
+        if (delegate instanceof CommittableWritableStates terminalStates) {
+            terminalStates.commit();
+        }
+    }
+}
---
+++ web3/src/main/java/com/swirlds/state/spi/WrappedWritableKVState.java
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package com.swirlds.state.spi;
+
+import jakarta.annotation.Nonnull;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentMap;
+
+@SuppressWarnings("deprecation")
+/**
+ * An implementation of {@link WritableKVState} that delegates to another {@link WritableKVState} as
+ * though it were the backend data source. Modifications to this {@link WrappedWritableKVState} are
+ * buffered, along with reads, allowing code to rollback by simply throwing away the wrapper.
+ *
+ * @param <K> The key
+ * @param <V> The value
+ */
+public class WrappedWritableKVState<K, V> extends WritableKVStateBase<K, V> {
+    /**
+     * The {@link WritableKVState} to delegate to for all read operations on cache miss, and for
+     * committing changes
+     */
+    private final WritableKVState<K, V> delegate;
+
+    /**
+     * Create a new instance that will treat the given {@code delegate} as the backend data source.
+     * Note that the lifecycle of the delegate <b>MUST</b> be as long as, or longer than, the
+     * lifecycle of this instance. If the delegate is reset or decommissioned while being used as a
+     * delegate, bugs will occur.
+     *
+     * @param delegate The delegate. Must not be null.
+     */
+    public WrappedWritableKVState(
+            @Nonnull final WritableKVState<K, V> delegate,
+            @Nonnull final ConcurrentMap<K, V> readCache,
+            @Nonnull Map<K, V> modifications) {
+        super(delegate.getStateKey(), readCache, modifications);
+        this.delegate = Objects.requireNonNull(delegate);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void putIntoDataSource(@Nonnull K key, @Nonnull V value) {
+        delegate.put(key, value);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void removeFromDataSource(@Nonnull K key) {
+        delegate.remove(key);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected V readFromDataSource(@Nonnull K key) {
+        return delegate.get(key);
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    @Override
+    protected Iterator<K> iterateFromDataSource() {
+        return delegate.keys();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long sizeOfDataSource() {
+        return delegate.size();
+    }
+}
